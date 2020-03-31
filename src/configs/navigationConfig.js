b9e78f62f02f759257e542b3a4bbcabf08352642
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2020-03-31 00:50:49 -0400

    Adds heatmap with sample data

--- src/configs/navigationConfig.js
+++ src/configs/navigationConfig.js
@@ -21,7 +21,6 @@ const navigationConfig = [
     id: "map",
     title: "Map",
     type: "item",
-    // icon??
     // permissions: [] // TODO: what are the options here?
     navLink: "/map"
   }
--- src/views/pages/MapComponent.js
+++ src/views/pages/MapComponent.js
@@ -1,20 +1,125 @@
 import React from 'react';
 import { Map, TileLayer } from 'react-leaflet';
+import HeatmapLayer from 'react-leaflet-heatmap-layer';
+import { addressPoints } from '../../example/realworld.10000.js';
 
 // TODO: would be better to name this component something else, so that that publicly displayed url path can be consistent with the component name
 class MapComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mapHidden: false,
+      layerHidden: false,
+      addressPoints,
+      radius: 4,
+      blur: 8,
+      max: 0.5,
+      limitAddressPoints: true,
+    };
+  }
+
+  /**
+   * Toggle limiting the address points to test behavior with refocusing/zooming when data points change
+   */
+  toggleLimitedAddressPoints() {
+    if (this.state.limitAddressPoints) {
+      this.setState({ addressPoints: addressPoints.slice(500, 1000), limitAddressPoints: false });
+    } else {
+      this.setState({ addressPoints, limitAddressPoints: true });
+    }
+  }
+
   render() {
+    if (this.state.mapHidden) {
+      return (
+        <div>
+          <input
+            type="button"
+            value="Toggle Map"
+            onClick={() => this.setState({ mapHidden: !this.state.mapHidden })}
+          />
+        </div>
+      );
+    }
+
+    const gradient = {
+      0.1: '#89BDE0', 0.2: '#96E3E6', 0.4: '#82CEB6',
+      0.6: '#FAF3A5', 0.8: '#F5D98B', '1.0': '#DE9A96'
+    };
+
     return (
-      <Map
-        style={{ width: '80%', height: '80vh' }}
-        center={[0, 0]}
-        zoom={13}
-      >
-        <TileLayer
-          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      <div>
+        <Map center={[0, 0]} zoom={13} style={{ width: '80%', height: '80vh' }}>
+          {!this.state.layerHidden &&
+              <HeatmapLayer
+                fitBoundsOnLoad
+                fitBoundsOnUpdate
+                points={this.state.addressPoints}
+                longitudeExtractor={m => m[1]}
+                latitudeExtractor={m => m[0]}
+                gradient={gradient}
+                intensityExtractor={m => parseFloat(m[2])}
+                radius={Number(this.state.radius)}
+                blur={Number(this.state.blur)}
+                max={Number.parseFloat(this.state.max)}
+              />
+            }
+          <TileLayer
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </Map>
+        <input
+          type="button"
+          value="Toggle Map"
+          onClick={() => this.setState({ mapHidden: !this.state.mapHidden })}
         />
-      </Map>
+        <input
+          type="button"
+          value="Toggle Layer"
+          onClick={() => this.setState({ layerHidden: !this.state.layerHidden })}
+        />
+        <input
+          type="button"
+          value="Toggle Limited Data"
+          onClick={this.toggleLimitedAddressPoints.bind(this)}
+        />
+
+        <div>
+          Radius
+          <input
+            type="range"
+            min={1}
+            max={40}
+            value={this.state.radius}
+            onChange={(e) => this.setState({ radius: e.currentTarget.value })}
+          /> {this.state.radius}
+        </div>
+
+        <div>
+          Blur
+          <input
+            type="range"
+            min={1}
+            max={20}
+            value={this.state.blur}
+            onChange={(e) => this.setState({ blur: e.currentTarget.value })}
+          /> {this.state.blur}
+        </div>
+
+        <div>
+          Max
+          <input
+            type="range"
+            min={0.1}
+            max={3}
+            step={0.1}
+            value={this.state.max}
+            onChange={(e) => this.setState({ max: e.currentTarget.value })}
+          /> {this.state.max}
+        </div>
+      </div>
     );
   }
 }
