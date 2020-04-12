3af1a06e94353abedf8cc736453b98411556da75
ohitsdoh <daniel.oh@heliolytics.com>
2020-04-12 15:25:25 -0400

    with remote server

--- src/views/pages/heatMap/heatMap.js
+++ src/views/pages/heatMap/heatMap.js
@@ -6,8 +6,9 @@ import "./heatMap.scss";
 import "leaflet/dist/leaflet.css"
 
 
-export default function HeatMap() {
+export default function HeatMap(props) {
   const fakeHeatData = { data: [] };
+  const [stateData, setStateData] = useState(null)
 
   const genFakeData = () => {
     return (Math.random() * (180+180)-180).toFixed(3) * 1;
@@ -20,6 +21,7 @@ export default function HeatMap() {
     });
     L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
       maxZoom:15,
+      minZoom:8,
       tileSize:200,
       zoomOffset:-1,
     }).addTo(map);
@@ -31,26 +33,42 @@ export default function HeatMap() {
       .on("locationerror", e => {
         console.log('user not found')
       });
-    // add heatmap layer
-    for (let i=0; i < 100; i++) {
-      let coords = {
-        lat: genFakeData(), 
-        lon: genFakeData(),
-        count:Math.round(Math.random()*100)
-      }
-      fakeHeatData.data.push(coords);
+    // fetch data from server to serve up state data
+    const fetchStatePositives = async () => {
+      const getPayload = {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      };
+
+      await fetch(`http://127.0.0.1:5000/get_all_states_positive`, getPayload)
+        .then(res => res.json())
+        .then(json => setStateData(json))
+        .catch(e => console.log(e))
     };
-    const heatLayer = new HeatmapOverlay({
-      radius:2,
-      maxOpacity:0.8,
-      scaleRadius:true,
-      useLocalExtrema:true,
-      latField:"lat",
-      lngField:"lon",
-      valueField:"count"
-    });
-    heatLayer.setData(fakeHeatData);
-    map.addLayer(heatLayer)
+    fetchStatePositives();
+    
+    // add heatmap layer
+    // for (let i=0; i < 100; i++) {
+    //   let coords = {
+    //     lat: genFakeData(), 
+    //     lon: genFakeData(),
+    //     count:Math.round(Math.random()*100)
+    //   }
+    //   fakeHeatData.data.push(coords);
+    // };
+    // const heatLayer = new HeatmapOverlay({
+    //   radius:2,
+    //   maxOpacity:0.8,
+    //   scaleRadius:true,
+    //   useLocalExtrema:true,
+    //   latField:"lat",
+    //   lngField:"lon",
+    //   valueField:"count"
+    // });
+    // heatLayer.setData(fakeHeatData);
+    // map.addLayer(heatLayer)
   }, []);
 
   return (
--- src/views/pages/profile/components/TitleSection.js
+++ src/views/pages/profile/components/TitleSection.js
@@ -3,16 +3,16 @@ import "../profile.scss";
 
 
 const ProfileTitleSection = props => (
-    <div className="titleSection">
-        <div className="titleSectionGroup">
-            <img src={props.Image} />
-            <div className="titleInfo">
-                <h1>{props.name}</h1>
-                <p>{props.joinDate}</p>
-                {/* <button>Edit Profile</button> */}
-            </div>
-        </div>
+  <div className="titleSection">
+    <div className="titleSectionGroup">
+      <img src={props.Image} />
+      <div className="titleInfo">
+          <h1>{props.name}</h1>
+          <p>{props.joinDate}</p>
+          {/* <button>Edit Profile</button> */}
+      </div>
     </div>
+  </div>
     
 )
 export default ProfileTitleSection
