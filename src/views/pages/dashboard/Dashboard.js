a7692972f5bd7363f4c70013263e8296e39211f9
Jared <jarmeyers@gmail.com>
2020-04-26 16:14:14 -0400

    Added coming soon text to HumanAPI button on click.

--- src/views/pages/dashboard/Dashboard.js
+++ src/views/pages/dashboard/Dashboard.js
@@ -7,7 +7,7 @@ import { setAuth } from "redux/actions/auth/authAction";
 import Questionnaire from "../questionnaire/Questionnaire"
 import HeatMap from "../heatMap/heatMap"
 const Dashboard = (props) => {
-
+    const [comingSoon, setComingSoon] = useState(false);
     return (<Container>
         <Row className="mx-auto h-100">
           <Card className="col-12 col-lg-5 mx-auto">
@@ -19,7 +19,9 @@ const Dashboard = (props) => {
             <CardBody>
                 <h2>Wearable Data</h2>
                 <p>Connecting your Fitbit, Apple Watch, or other wearable device will allow us to show you more personalized insights.</p>
-                <Button className="text-white mx-auto font-weight-bold align-self-center mt-2" block={true} color="primary">Connect to HumanAPI</Button>
+                <p className={comingSoon ? '' : 'd-none'}>Coming soon!</p>
+                <Button className="text-white mx-auto font-weight-bold align-self-center mt-2" block={true} color="primary"
+                    onClick={() => setComingSoon(true)}>Connect to HumanAPI</Button>
             </CardBody>
           </Card>
         </Row>
