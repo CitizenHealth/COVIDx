3684647aa75ad045a5e112c06056be3397e5b87d
Unwana Essien <unwana.essien@outlook.com>
2020-05-20 21:57:14 +0100

    Get styling for form

--- src/components/Tripetto/QuestionForm.js
+++ src/components/Tripetto/QuestionForm.js
@@ -6,10 +6,12 @@ const token = process.env.REACT_APP_TRIPETTO_TOKEN
 
 export const QuestionForm = () => {
   const [ formDefinition, setFormDefinition] = useState();
+  const [ formStyle, setFormStyle ] = useState();
   const [ loading, setLoading ] = useState(true)
   const [ error, setError ] = useState(false)
   useEffect(() => {
     Services.init({ token });
+    Services.style.then(res => { setFormStyle(res) });
     Services.definition.then(res => {
       setLoading(false)
       setFormDefinition(res);
@@ -26,6 +28,7 @@ export const QuestionForm = () => {
       { formDefinition && (
         <Collector
           definition={formDefinition}
+          style={formStyle}
           onFinish={(instance) => {
             console.log(instance);
           }}
