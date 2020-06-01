6721d8e48f1a5be9210f1f1fc0a675dd33648b0e
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2020-05-31 20:45:38 -0400

    placed Tripetto questionnaire

--- src/views/pages/questionnaire/Questionnaire.js
+++ src/views/pages/questionnaire/Questionnaire.js
@@ -1,11 +1,37 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "rc-slider/assets/index.css";
 import "../../../assets/scss/plugins/extensions/slider.scss";
 import { UserContext } from "App";
+import styled from "styled-components";
+import * as TripettoCollectorRolling from "tripetto-collector-rolling";
+import TripettoServices from "tripetto-services";
+
+const QuestionForm = styled.div`
+  background-color: white;
+  height: 80vh;
+  > .tripetto-collector-rolling {
+    width: inherit;
+    height: inherit;
+    position: relative;
+  }
+`;
+const token = process.env.REACT_APP_TRIPETTO_HEALTH_REPORT_TOKEN;
 
 const Questionnaire = (props) => {
-  const user = useContext(UserContext);
-  return <div></div>;
+  useEffect(() => {
+    TripettoServices.init({
+      token,
+    });
+
+    TripettoCollectorRolling.run({
+      element: document.getElementById("survey"), // Or supply your own element here
+      definition: TripettoServices.definition,
+      style: TripettoServices.style,
+      onFinish: TripettoServices.onFinish,
+      onAttachment: TripettoServices.onAttachment,
+    });
+  });
+  return <QuestionForm id="survey"></QuestionForm>;
 };
 
 export default Questionnaire;
