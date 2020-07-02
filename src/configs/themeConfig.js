8d72e21c7ec5f96537309319bc45044fe73c00b4
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2020-07-02 19:37:18 -0400

    Navbar is now sticky

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -6,7 +6,7 @@ const themeConfig = {
   theme: "light", // options[String]: 'light'(default), 'dark', 'semi-dark'
   sidebarCollapsed: false, // options[Boolean]: true, false(default)
   navbarColor: "default", // options[String]: default / primary / success / danger / info / warning / dark
-  navbarType: "floating", // options[String]: floating(default) / static / sticky / hidden
+  navbarType: "sticky", // options[String]: floating(default) / static / sticky / hidden
   footerType: "static", // options[String]: static(default) / sticky / hidden
   disableCustomizer: true, // options[Boolean]: true, false(default)
   hideScrollToTop: false, // options[Boolean]: true, false(default)
