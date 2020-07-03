3407a4ce7552f242034699c1081ff80874fd02e5
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2020-07-02 20:09:13 -0400

    Firebase API key no longer public information

--- src/components/Tripetto/QuestionForm.js
+++ src/components/Tripetto/QuestionForm.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { Collector } from "tripetto-collector-rolling";
 import Services from "tripetto-services";
 
-const token = process.env.REACT_APP_TRIPETTO_TOKEN
+const token = process.env.REACT_APP_TRIPETTO_TOKEN;
 
 const FormContainer = styled.div`
   height: 80vh;
@@ -61,5 +61,5 @@ export const QuestionForm = () => {
       </div>
     );
   }
-  
+
 }
