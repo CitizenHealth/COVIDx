029839bffc9eeae3932631acd782f2a48a147785
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2020-07-23 22:47:31 -0400

    updated parsingRules

--- src/views/pages/questionnaire/questions/utils.js
+++ src/views/pages/questionnaire/questions/utils.js
@@ -40,19 +40,87 @@ export const parsingRules = {
     );
     return result;
   },
-  emotional_health: (fields) => {},
-  testing_questions: (fields) => {},
-  personal_decisions: (fields) => {},
-  home_conditions: (fields) => {},
-  work_conditions: (fields) => {},
-  city_state_decisions: (fields) => {},
+  testing_questions: (fields) => {
+    const multiple_choice_fields = ["current_symptoms"];
+    const date_fields = ["self_date_tested", "household_date_tested"];
+    let result = extractAnswers(
+      fields,
+      multiple_choice_fields,
+      [],
+      date_fields
+    );
+    return result;
+  },
+  emotional_health: (fields) => {
+    const multiple_choice_fields = [
+      "current_symptoms",
+      "mental_illnesses",
+      "events_worries",
+      "stress_reducing_methods",
+    ];
+    let result = extractAnswers(fields, multiple_choice_fields);
+    return result;
+  },
+  personal_decisions: (fields) => {
+    const multiple_choice_fields = [
+      "current_symptoms",
+      "protective_measures_left_home",
+      "protective_measures_enter_home",
+      "exercises",
+      "diets",
+      "food_avoiding",
+    ];
+    const numeric_fields = ["how_do_you_feel_leaving_house"];
+    let result = extractAnswers(
+      fields,
+      multiple_choice_fields,
+      [],
+      [],
+      numeric_fields
+    );
+    return result;
+  },
+  home_conditions: (fields) => {
+    const multiple_choice_fields = ["current_symptoms"];
+    let result = extractAnswers(fields, multiple_choice_fields);
+    return result;
+  },
+  work_conditions: (fields) => {
+    const multiple_choice_fields = ["current_symptoms", "industry"];
+    let result = extractAnswers(fields, multiple_choice_fields);
+    return result;
+  },
+  city_state_decisions: (fields) => {
+    const multiple_choice_fields = ["current_symptoms"];
+    const numeric_fields = ["governor_orders_rating"];
+    let result = extractAnswers(
+      fields,
+      multiple_choice_fields,
+      [],
+      [],
+      numeric_fields
+    );
+    return result;
+  },
 };
 
 const extractAnswers = (
   fields,
   multiple_choice_fields = [],
-  multi_items_string_fields = []
+  multi_items_string_fields = [],
+  date_fields = [],
+  numeric_fields = []
 ) => {
+  /**
+   * A reusable function to extract essential data form survey response.
+   * @param  {Array} fields                     The raw data got from the survey response.
+   * @param  {Array} multiple_choice_fields     Labels for fields that allow multiple selections other the 'other' fields
+   * @param  {Array} multi_items_string_fields  Labels for string fields that describe multiple items
+   * @param  {Array} date_fields                Text fields with date values.
+   * @param  {Array} numeric_fields             Text fields encoded in numeric values.
+   * @return {Object}                           Reduced object that only contains the necessary data.
+   */
+
   let multiple_choice_ranges = [];
   let lowerEnd = -1;
   let finalResult = {};
@@ -63,6 +131,7 @@ const extractAnswers = (
         field.type === "tripetto-block-checkboxes") &&
       typeof field.value === "boolean"
     ) {
+      // creating the range for multiple choice fields
       if (lowerEnd === -1) {
         lowerEnd = i;
       }
@@ -70,12 +139,23 @@ const extractAnswers = (
         multiple_choice_ranges.push([lowerEnd, i]);
         lowerEnd = -1;
       }
+    } else if (field.type === "tripetto-block-yes-no") {
+      finalResult[field.name] = field.value === "yes" ? true : false;
     } else {
       if (field.name === "how_are_you_feeling") {
         finalResult[field.name] = parseInt(field.value);
       } else if (field.name === "fever_best_guess") {
         if (field.value) {
           finalResult[field.name] = parseInt(field.value);
+        }
+      } else if (date_fields.includes(field.name)) {
+        if (field.value !== undefined) {
+          const date = new Date(field.string);
+          if (date !== "Invalid Date") {
+            finalResult[field.name] = date;
+          } else {
+            throw new Error("invalid date value at ", field.name);
+          }
         }
       } else finalResult[field.name] = field.value;
     }
@@ -93,7 +173,9 @@ const extractAnswers = (
       choices[choices.length - 1].toLowerCase() === "other"
     ) {
       choices.pop();
-      const other_groups = finalResult[other_label].split(/;\s*/g);
+      const other_groups = finalResult[other_label]
+        .split(/;\s*/g)
+        .map((str) => str.toLowerCase().replace(/[\s|-]+|-/g, "_"));
       choices.push(...other_groups);
     }
     if (finalResult.hasOwnProperty(other_label)) {
@@ -113,6 +195,15 @@ const extractAnswers = (
         .split(/;\s*/g)
         .map((str) => str.toLowerCase().replace(/[\s|-]+|-/g, "_"));
       finalResult[field] = items;
+    }
+  });
+
+  // process numeric fields
+  numeric_fields.forEach((field) => {
+    if (finalResult[field] !== undefined) {
+      if (finalResult[field].includes("."))
+        finalResult[field] = parseFloat(finalResult[field]);
+      else finalResult[field] = parseInt(finalResult[field]);
     }
   });
 
